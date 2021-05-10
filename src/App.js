c7a2756682c3890dfeafcf0d281ec17251b1d771
Fredrik <fredrikf2000@gmail.com>
2021-05-11 00:49:39 +0200

    styled post and added sass

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import Post from "./components/Post";
 
-import "./App.css";
-
 function App() {
   return (
     <div className="app">
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,18 @@
+import droptop from "../assets/droptop.jpg";
+
 const Post = () => {
   return (
-    <div>
+    <div className="post">
       <h3>Username</h3>
+      {/* header, username and AVI */}
+
+      <img className="post__image" src={droptop} alt="" />
+      {/* image */}
+
+      <h4>
+        _flyxo: Drop Top remix dropping soon! @whensdayod @nicktaylorlife!
+      </h4>
+      {/*username and caption */}
     </div>
   );
 };
