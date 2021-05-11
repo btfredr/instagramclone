120f950caa1a2940e80b78c70e95188c925ff345
Fredrik <fredrikf2000@gmail.com>
2021-05-11 20:47:11 +0200

    started working on login modal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import { useState, useEffect } from "react";
 import Post from "./components/Post";
 import { db } from "./firebase";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import { Button, Input } from "@material-ui/core";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   useEffect(() => {
     db.collection("posts").onSnapshot((snapshot) => {
@@ -16,8 +48,46 @@ function App() {
     });
   }, []);
 
+  const signUp = (event) => {};
+
   return (
     <div className="app">
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <form>
+            <center>
+              <img
+                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                className="app__headerImage"
+                alt="Instagram logo"
+              />
+            </center>
+
+            <Input
+              placeholder="Username"
+              type="text"
+              value={email}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+
+            <Input
+              placeholder="Email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <Input
+              placeholder="Password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <Button onClick={signUp}>Sign Up</Button>
+          </form>
+        </div>
+      </Modal>
       <div className="app__header">
         <img
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
@@ -26,6 +96,7 @@ function App() {
         />
       </div>
 
+      <Button onClick={() => setOpen(true)}>Sign Up</Button>
       <h1>Hello, welcome to my Instagram Clone!</h1>
 
       {posts.map(({ id, post }) => (
